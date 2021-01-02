e351cfed9124a3ddebcaa609f7feb556d3873668
Moleja27 <monicsalejandra887@gmail.com>
2021-01-01 21:11:30 -0500

    Prueba

---
+++ javascript.js
@@ -0,0 +1,72 @@
+
+
+  /*var users = [
+    {
+        name: 'Erik',
+        age: 29,
+        email: 'erik@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/erik' },
+            { name: 'twitter', url: 'twitter/erik' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Georg',
+        age: 33,
+        email: 'georg@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/georg' },
+            { name: 'twitter', url: 'twitter/georg' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Andrea',
+        age: 42,
+        email: 'andrea@hotmail.com',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+    {
+        name: 'Oscar',
+        age: 31,
+        email: 'oscar@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/oscar' },
+            { name: 'twitter', url: 'twiter/oscar' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Daniela',
+        age: 22,
+        email: 'andrea@uaq.mx',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+];*/
+
+const Perros = {
+    raza: ["Pator Aleman", "san bernardo", "salchicha"],
+    edad: 3,
+    peso: "50 kilogramos"
+    
+}
+
+const newPerro = Perros.raza.map((r) => r);
+const dog = Perros.raza.map(function(r){
+    return r
+})
+
+console.log(newPerro)
+console.log(dog)
+
+
+
